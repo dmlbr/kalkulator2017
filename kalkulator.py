d0a0839f0e0e61084b58fcdf979343db4b388ba0
Dimitr <student@student.ue.poznan.pl>
2017-04-11 19:25:42 +0200

    funkcja z pomocą gotowa do review

--- kalkulator.py
+++ kalkulator.py
@@ -3,4 +3,10 @@ def dolodowanie():
 	b = int(input('podaj pirwsx licbe'))
 	print(a+b)
 	
+	
+	
+def get_help():
+print(" Doasda a kajsdl lakjsdlkjasd  laksjd  klajslkdjlak dk")
+
+get_help()
 dolodowanie()
